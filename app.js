fef0c9868997738b3af3d290a87f21d157ec3575
suhita4 <63366417+suhita4@users.noreply.github.com>
2022-03-08 17:11:52 +0530

    frontend

--- app.js
+++ app.js
@@ -1,28 +1,52 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+
+
 const taskRoutes = require('./routes/taskRoutes');
 
 const app = express();
 
-const dbURI = 'mongodb+srv://stige:mongodb@task-manager.bkc32.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const dbURI = 'mongodb+srv://stige:trial@lms.kkiqf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
-    app.listen(process.env.PORT || 5000);
+    app.listen(process.env.PORT || 8080);
   })
   .catch(err => console.log(err));
 
 
 app.set('view engine', 'ejs');
-app.use("/public", express.static(__dirname + "/public"));
+app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(cookieParser());
 
 
-app.get('/', (req, res) => {
-  res.redirect('/tasks');
+app.get('/', function (req, res) {
+  res.render('home');
 });
 
+app.get('/signUp', function (req, res) {
+  res.render('signUp');
+});
 
-app.use(taskRoutes);
+app.get('/signIn', function (req, res) {
+  res.render('signIn');
+});
+
+app.get('/dashboard', function (req, res) {
+  res.render('dashboard');
+});
+
+app.get('/course', function (req, res) {
+  res.render('course');
+});
+
+app.get('/tasks', function (req, res) {
+  res.render('tasks');
+});
+
+app.use('/tasks', taskRoutes);
---
+++ controllers/taskController.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+
+const Task = require('../models/task');
+
+
+function allTasks(req, res) {
+
+  const token = req.cookies.jwt;
+
+  if (token) {
+    jwt.verify(token, 'User', (err, decodedToken) => {
+      if (err) {
+        res.cookie('jwt', '', { maxAge: 1 });
+        res.redirect('/signIn');
+        next();
+      } else {
+        Task.findById(decodedToken.id)
+          .then((result) => {
+            console.log("All tasks: ", result);
+            res.render('tasks', { title: 'All Tasks', tasks: result });
+          })
+          .catch(errr => console.log(errr));
+      }
+    })
+  } else {
+    res.cookie('jwt', '', { maxAge: 1 });
+    res.redirect('/signUp');
+  }
+
+
+}
+
+module.exports = allTasks;
