5375648f180a85fda72f781d6f266252ecaa8b7e
suhita4 <63366417+suhita4@users.noreply.github.com>
2022-03-09 22:48:44 +0530

    Dash working

--- controllers/authController.js
+++ controllers/authController.js
@@ -35,6 +35,25 @@ const handleErrors = (err) => {
   return errors;
 }
 
+
+const updateErrors = (err) => {
+  console.log(err.message, err.code);
+  let errors = { email: '', password: '' };
+
+  // validation errors
+  if (err.message.includes('user validation failed')) {
+    // console.log(err);
+    Object.values(err.errors).forEach(({ properties }) => {
+      // console.log(val);
+      // console.log(properties);
+      errors[properties.path] = properties.message;
+    });
+  }
+
+  return errors;
+}
+
+
 // create json web token
 const maxAge = 3 * 24 * 60 * 60;
 const createToken = (id) => {
@@ -53,6 +72,26 @@ module.exports.signin_get = (req, res) => {
 }
 
 module.exports.signup_post = async (req, res) => {
+  const { name, email, username, password } = req.body;
+
+  try {
+    const user = await User.create({ name, email, username, password });
+    const token = createToken(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({ user: user._id });
+  }
+  catch(err) {
+    const errors = updateErrors(err);
+    res.status(400).json({ errors });
+  }
+ 
+}
+
+module.exports.update_get = (req, res) => {
+  res.render('dashboard');
+}
+
+module.exports.update_post = async (req, res) => {
   const { name, email, username, password } = req.body;
 
   try {
